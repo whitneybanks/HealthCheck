7ab939737479c7d8ca0e68933a7e0b31b1ebf86d
whitneybanks <whitneydeaon@gmail.com>
2021-03-19 21:08:26 -0500

    Healthcheck angular components

--- AngularSpa/Controllers/OidcConfigurationController.cs
+++ AngularSpa/Controllers/OidcConfigurationController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace AngularSpa.Controllers
